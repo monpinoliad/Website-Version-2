dd0cabc1d29addf9802819d1ad103fd8184d90d8
monpinoliad <monpinoliad@gmail.com>
2019-10-21 06:33:05 +0800

    Added nav and header

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import React from 'react';
 function App() {
   return (
     <div className="App">
-      Hello
+      <div className="nav">
+        <div className="nav__title">Sholomon Pinoliad</div>
+      </div>
+      <header className="header">
+        <div className="header__image"></div>
+        <div className="header__information">
+          <div className="header__information--container">
+            <h1>I am Sholomon Pinoliad</h1>  
+            <h2>Motivated, Outgoing, Confident</h2>
+          </div>
+        </div>
+      </header>
+      <section className="section">
+        In ipsum laboris occaecat commodo tempor sint cupidatat.
+      </section>
     </div>
   );
 }
